856064ab5b9a353849319ae9e9d97841a3bc1304
ritikrana0169 <saviji520@gmail.com>
2023-06-17 18:35:31 +0530

    product menu done customer menu done

--- SB201TeamProject/src/main/resources/static/AdminSide/orderManagement.js
+++ SB201TeamProject/src/main/resources/static/AdminSide/orderManagement.js
@@ -10,13 +10,41 @@ function handleOrderManagement() {
     <div class="productContainer">
     
     <div class="submenu" id="productSubMenu">
-    <button id="viewAllCustomersBtn">View Customers</button>
-    <button id="editRole">Edit Role</button>
-    <button id="deactivateCustomerBtn">Deactivate Customer</button>
-    <button id="searchByMailBtn">Search Customer</button>
+    <button id="viewAllOrdersBtn">View All Orders</button>
+    <button id="viewSingleOrderBtn">View Customer Orders</button>
+    <button id="removeOrderByIdBtn">Remove Order</button>
 </div>
 <div id="productContent"><h1 class="bouncing-text">Orders Menu</h1></div>
 
 </div>
 `;
+
+var viewAllOrdersBtn=document.getElementById("viewAllOrdersBtn");
+var viewSingleOrderBtn=document.getElementById("viewSingleOrderBtn");
+var removeOrderByIdBtn=document.getElementById("removeOrderByIdBtn");
+var productContainer=document.querySelector(".productContainer");
+
+//view All orders Handler
+viewAllOrdersBtn.addEventListener("click",()=>{
+    productContent.innerHTML="View All Clicked";
+})
+
+
+
+//view single order handler here
+viewSingleOrderBtn.addEventListener("click",()=>{
+    productContent.innerHTML="View single order  Clicked";
+})
+
+
+
+//remove order handler
+removeOrderByIdBtn.addEventListener("click",()=>{
+    productContent.innerHTML="remove order  Clicked";
+})
+
+
+
+
+
 }
