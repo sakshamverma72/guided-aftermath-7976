ba027411a9f5eebea2e26e6b61b95e375de8ca3b
Aayush Kindo <aayush.kindo@gmail.com>
2023-06-18 18:23:44 +0530

    completed 2 pages with api  functionalty user and order

--- SB201TeamProject/src/main/resources/static/UserSide/order/order.js
+++ SB201TeamProject/src/main/resources/static/UserSide/order/order.js
@@ -5,11 +5,12 @@ const categoryBtn = document.getElementById('categoryBtn');
 const logoutBtn = document.getElementById('logoutBtn');
 const content=document.getElementById("content");
 // Add event listeners to the buttons
-productBtn.addEventListener('click',  displayProducts);
+productBtn.addEventListener('click',  handleAddProduct);
 //customerBtn.addEventListener('click', handleCustomerManagement);
-orderBtn.addEventListener('click', removeProducts);
-categoryBtn.addEventListener('click',displayProductsall );
+orderBtn.addEventListener('click', handleDeleteProduct);
+categoryBtn.addEventListener('click',handleViewProducts );
 //logoutBtn.addEventListener('click', handleLogout);
+const apiUrl = 'https://fakestoreapi.com/products'; //replace api
 
 function displayProducts() {
     // Array of products
@@ -107,3 +108,144 @@ function displayProducts() {
     });
   }
   
+ // viewProductsBtn.addEventListener('click', handleViewProducts);
+
+  function handleViewProducts() {
+      console.log('View Products button clicked');
+     content.innerHTML="";
+     content.innerHTML=` <div class="viewAllProducts">
+     <h2>Sweets List</h2>
+     <table id="productTable">
+       <tr style="position:sticky; top:0%; background-color:#111011;">
+       <th>Id</th>
+         <th>Name</th>
+         <th>Image</th>
+         <th>Price</th>
+         <th>Description</th>
+         <th>Available</th>
+         <th>Category</th>
+       </tr>
+     </table>
+   </div>`;
+   fetchProducts();
+   function fetchProducts() {
+      fetch(apiUrl)
+        .then(function(response) {
+          return response.json();
+        })
+        .then(function(data) {
+          var table = document.getElementById('productTable');
+          // table.innerHTML = '';
+  
+          for (var i = 0; i < data.length; i++) {
+            
+            var product = data[i];
+  
+             var row = table.insertRow();
+             var prodId=row.insertCell();
+             prodId.innerText=product.id;
+            var nameCell = row.insertCell();
+            nameCell.innerText = product.name;
+  
+            var imageCell = row.insertCell();
+            
+            imageCell.innerHTML = '<img src="' + product.image + '" alt="' + product.name + '">';
+      
+  
+            var priceCell = row.insertCell();
+            priceCell.innerText = product.price;
+  
+            var descriptionCell = row.insertCell();
+            descriptionCell.innerText = product.description;
+  
+            var availableCell = row.insertCell();
+            availableCell.innerText = product.available ? 'Yes' : 'No';
+  
+            var categoryCell = row.insertCell();
+            categoryCell.innerText = product.category;
+  
+            
+          }
+        })
+        .catch(function(error) {
+          console.error('Error:', error);
+        });
+  
+  }
+  }
+
+
+  function handleDeleteProduct() {
+    console.log('Delete Product button clicked');
+    content.innerHTML=`
+    <div class="container">
+    <h2>Cancel Sweet</h2>
+  <form id="deleteForm">
+    <div class="form-group">
+      <label for="productId">Product ID:</label>
+      <input type="number" id="productId" name="productId" required>
+    </div>
+    <button type="submit">Delete</button>
+  </form></div>`;
+
+  document.getElementById("deleteForm").addEventListener("submit",(e)=>{ 
+    e.preventDefault();
+    var deleteId=document.getElementById("productId").value;
+  fetch(`${apiUrl}/${deleteId}`, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json'
+      // Add any other headers required by your API
+    }
+  })
+    .then(response => {
+      if (response.ok) {
+        console.log('Item deleted successfully.');
+        alert('Item deleted successfully. '+deleteId);
+      } else {
+
+        console.log('Failed to delete item.');
+        alert('Product Not Found Id -> '+deleteId)
+      }
+    })
+    .catch(error => {
+      console.error('Error:', error);
+    });})}   
+
+
+    function handleAddProduct() {
+      console.log('Add Product button clicked');
+      content.innerHTML=`
+      <div class="container">
+      <h2>Add Sweet</h2>
+    <form id="deleteForm">
+      <div class="form-group">
+        <label for="productId">Product ID:</label>
+        <input type="number" id="productId" name="productId" required>
+      </div>
+      <button type="submit">Add</button>
+    </form></div>`;
+  
+    document.getElementById("deleteForm").addEventListener("submit",(e)=>{ 
+      e.preventDefault();
+      var deleteId=document.getElementById("productId").value;
+    fetch(`${apiUrl}/${deleteId}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+        // Add any other headers required by your API
+      }
+    })
+      .then(response => {
+        if (response.ok) {
+          console.log('Item added successfully.');
+          alert('Item added successfully. '+deleteId);
+        } else {
+  
+          console.log('Failed to add item.');
+          alert('Product Not Found Id -> '+deleteId)
+        }
+      })
+      .catch(error => {
+        console.error('Error:', error);
+      });})}
--- SB201TeamProject/src/main/resources/static/UserSide/user/user.js
+++ SB201TeamProject/src/main/resources/static/UserSide/user/user.js
@@ -4,11 +4,13 @@ const orderBtn = document.getElementById('orderBtn');
 const categoryBtn = document.getElementById('categoryBtn');
 const logoutBtn = document.getElementById('logoutBtn');
 const content=document.getElementById("content");
+const customerId = 3;
+const customerApi='https://648c85bd8620b8bae7ed09b3.mockapi.io/customers/3';
 // Add event listeners to the buttons
 productBtn.addEventListener('click',  displayProducts);
-customerBtn.addEventListener('click',removeProducts);
-orderBtn.addEventListener('click', displayProductsall);
-categoryBtn.addEventListener('click',displayProductsall );
+customerBtn.addEventListener('click',handledeactivateCustomerBtn);
+orderBtn.addEventListener('click', handleviewAllCustomersBtn);
+//categoryBtn.addEventListener('click',displayProductsall );
 //logoutBtn.addEventListener('click', handleLogout);
 
 function displayProducts() {
@@ -107,3 +109,204 @@ function displayProducts() {
     });
   }
   
+  function handleviewAllCustomersBtn() {
+    console.log('View All Customers Clicked');
+    // Add your code to handle the Add Product action here
+    content.innerHTML="";
+    content.innerHTML=` <div class="viewAllProducts">
+    <h2>User Details</h2>
+    <table id="productTable">
+      <tr style="position:sticky; top:0%; background-color:#111011;">
+       <th>Id</th>
+        <th>Name</th>
+        <th>UserName</th>
+        <th>Status</th>
+        <th>Role</th>
+      </tr>
+    </table>
+  </div>`;
+  fetchProducts();
+  function fetchProducts() {
+     fetch(customerApi)
+       .then(function(response) {
+       
+        return response.json();
+       })
+       .then(function(data) {
+        console.log(data)
+         var table = document.getElementById('productTable');
+         // table.innerHTML = '';
+ 
+        
+           
+           var product = data;
+ 
+            var row = table.insertRow();
+            var customerId=row.insertCell();
+            customerId.innerText=product.customerId;
+           var nameCell = row.insertCell();
+           nameCell.innerText = product.name;
+ 
+           var mailCell = row.insertCell();
+           
+           mailCell.innerText = product.mailOrUsername;
+     
+ 
+           var acitveCell = row.insertCell();
+           acitveCell.innerText = product.active;
+ 
+           var roleCell = row.insertCell();
+           roleCell.innerText = product.role;
+ 
+           
+         
+       })
+       .catch(function(error) {
+         console.error('Error:', error);
+       });
+ 
+ }  
+}
+
+
+function handledeactivateCustomerBtn() {
+  console.log('handledeactivateCustomerBtn button clicked');
+  // Add your code to handle the Edit Product action here
+
+  content.innerHTML="";
+  content.innerHTML=`
+  <div class="container">
+  <h1>Delete User</h1>
+
+  <form id="deactivateForm">
+    <label for="customerId">Customer ID:</label>
+    <input type="text" id="customerId" name="customerId" required>
+
+    <button type="submit">Deactivate</button>
+  </form>
+  </div>`;
+
+
+
+  const form = document.querySelector('#deactivateForm');
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+
+const customerId = document.querySelector('#customerId').value;
+
+const url = `${customerApi}/${customerId}`;
+
+fetch(url, {
+  method: 'PATCH',
+  headers: {
+    'Content-Type': 'application/json'
+    // Add any other headers required by your API
+  },
+  body: JSON.stringify({ active: false }) 
+})
+  .then(response => {
+    if (response.ok) {
+      console.log('Customer deactivated successfully.');
+      alert('User deleted successfully.');
+    } else {
+      console.log('Failed to deactivate customer.');
+      alert('Failed to delete  customer.');
+    }
+  })
+  .catch(error => {
+    console.error('Error:', error);
+  });
+});
+
+}
+
+function handleSearchByMailBtn() {
+  console.log('handleSearchByMailBtn button clicked');
+  // Add your code to handle the Delete Product action here
+
+  content.innerHTML="";
+  content.innerHTML=` 
+  <div class="container">
+  <h1>Find User</h1>
+
+  <form id="lookupForm">
+    <label for="username">Username:</label>
+    <input type="text" id="username" name="username" required>
+
+    <button type="submit">Search</button>
+  </form></div>
+  <div id="result"></div>`
+
+  const form = document.querySelector('#lookupForm');
+const resultDiv = document.querySelector('#result');
+
+form.addEventListener('submit', (event) => {
+event.preventDefault();
+
+const username = document.querySelector('#username').value;
+
+const url = `${customerApi}/${1}`;                                //replace username here
+
+fetch(url)
+  .then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error('Failed to fetch user.');
+    }
+  })
+  .then(data => {
+    
+  //   const user = data.user;
+
+  //   resultDiv.textContent = `User found: ${user.name}`;
+  displaySingleUser(data);
+  })
+  .catch(error => {
+    console.error('Error:', error);
+    resultDiv.textContent = 'Failed to fetch user.';
+    alert('Failed to fetch user.');
+  });
+});
+
+
+function displaySingleUser(data){
+
+
+  content.innerHTML=` <div class="viewAllProducts">
+  <h2>User Found</h2>
+  <table id="productTable">
+    <tr style="position:sticky; top:0%; background-color:#111011;">
+     <th>Id</th>
+      <th>Name</th>
+      <th>UserName</th>
+      <th>Status</th>
+      <th>Role</th>
+    </tr>
+  </table>
+</div>`;
+var table = document.getElementById('productTable');
+  var product = data;
+
+          var row = table.insertRow();
+          var customerId=row.insertCell();
+          customerId.innerText=product.customerId;
+         var nameCell = row.insertCell();
+         nameCell.innerText = product.name;
+
+         var mailCell = row.insertCell();
+         
+         mailCell.innerText = product.mailOrUsername;
+   
+
+         var acitveCell = row.insertCell();
+         acitveCell.innerText = product.active;
+
+         var roleCell = row.insertCell();
+         roleCell.innerText = product.role;
+}
+
+
+
+}
